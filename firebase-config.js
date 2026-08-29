4de2e5fc0bdde9160f43cc35912ef5f04e5e6972
Lpedrassi <lucasaugustopedrassi@gmail.com>
2026-08-29 16:30:12 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Cole aqui as chaves do SEU projeto Firebase.
+// Onde encontrar: Console do Firebase > Configuracoes do projeto > "Seus apps" > app Web
+// Essas chaves NAO sao secretas - e normal (e seguro) elas aparecerem no codigo do site.
+// A seguranca de verdade fica nas "Regras" do Firestore (arquivo firestore.rules).
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAdDwfLLAhB3jiRtRRhWg9wKJbIogBrciQ",
+  authDomain: "adpf-7df40.firebaseapp.com",
+  projectId: "adpf-7df40",
+  storageBucket: "adpf-7df40.firebasestorage.app",
+  messagingSenderId: "65986016663",
+  appId: "1:65986016663:web:09fdcbbed6081ce45600d2"
+};
+
